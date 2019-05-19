ec3b2c8f23457266a15c9d890994d439a844786e
KrzysiekGL <krzysiek@twoflower.localdomain>
2019-05-20 01:20:19 +0200

    Create basic PID controller class

--- PID/src/pl/krzysiekgl/Main.java
+++ PID/src/pl/krzysiekgl/Main.java
@@ -3,6 +3,7 @@ package pl.krzysiekgl;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    PID myPID = new PID(10);
+        System.out.println(myPID.getTs());
     }
 }
--- PID/src/pl/krzysiekgl/PID.java
+++ PID/src/pl/krzysiekgl/PID.java
@@ -1,10 +1,52 @@
 package pl.krzysiekgl;
 
-public class PID {
-    private Integer Ts; //sample time in milliseconds
-    private Double Kp;  //proportional gain
-    private Double Ki;  //integral gain
-    private Double Kd;  //derivative gain
+public final class PID {
+    private int Ts; //sample time in milliseconds
+    private double Kp;  //proportional gain
+    private double Ki;  //integral gain
+    private double Kd;  //derivative gain
     
+    private double prevError;
+    private double integralVal;
     
+    public PID(int sampleTime, double proportionalGain, double integralGain, double derivativeGain) {
+        this.Ts = sampleTime;
+        this.Kp = proportionalGain;
+        this.Ki = integralGain;
+        this.Kd = derivativeGain;
+        
+        prevError = 0;
+        integralVal = 0;
+    }
+    
+    public PID(int sampleTime) {
+        this(sampleTime, 0, 999999, 0);
+    }
+    
+    //PID tuning
+    public void setKp(double kp) {
+        Kp = kp;
+    }
+    
+    public void setKi(double ki) {
+        Ki = ki;
+    }
+    
+    public void setKd(double kd) {
+        Kd = kd;
+    }
+    
+    //PID algorithm
+    public double calculate(double setPoint, double processValue) { //returns Control Variable
+        double controlVariable = 0;
+        double error = setPoint - processValue;
+        
+        //calculations
+        integralVal += error;
+        double derivative = (error - prevError) / Ts;
+        controlVariable = Kp * error + Ki * integralVal + Kd * derivative;
+        
+        prevError = error;
+        return controlVariable;
+    }
 }
