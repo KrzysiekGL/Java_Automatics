50bb4421d69a7f6a68d4912d068f99a523936bcb
KrzysiekGL <krzysiek@twoflower.localdomain>
2019-05-19 22:55:54 +0200

    Change project programming language froc C++ to Java

---
+++ PID/src/pl/krzysiekgl/Main.java
@@ -0,0 +1,8 @@
+package pl.krzysiekgl;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ PID/src/pl/krzysiekgl/PID.java
@@ -0,0 +1,10 @@
+package pl.krzysiekgl;
+
+public class PID {
+    private Integer Ts; //sample time in milliseconds
+    private Double Kp;  //proportional gain
+    private Double Ki;  //integral gain
+    private Double Kd;  //derivative gain
+    
+    
+}
