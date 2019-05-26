7995c31258485c09d1234084ccd560a4ce0cdbe0
KrzysiekGL <krzysiek@twoflower.localdomain>
2019-05-26 14:18:11 +0200

    Update README.md

--- PID/src/pl/krzysiekgl/Main.java
+++ PID/src/pl/krzysiekgl/Main.java
@@ -1,26 +1,30 @@
 package pl.krzysiekgl;
 
 
-import static java.lang.Math.pow;
-import static java.lang.Math.sin;
-
 public class Main {
 
     public static void main(String[] args) {
         
         Derivative S = new Derivative(0.1);
-        Derivative S2 = new Derivative(0.1);
+        Integral I = new Integral(0.1);
+        
+        Gain kp = new Gain(0.2);
+        Gain ki = new Gain(0.1);
+        Gain kd = new Gain(0);
         
         double time = 0;
-        //function f(t) = t
+        
+        double foo = 1;
+        double procV = 0;
         
         while(true) {
-            double functionVal = pow(time, 3);
-            double result = S.differentiates(S2.differentiates(functionVal));
-            System.out.println(time + ";" + functionVal + ";" + result);
+            double err = foo - procV;
+            procV = kp.product(err) + ki.product(I.add(err)) + kd.product(S.differentiates(err));
+            
+            System.out.println(time + ";" + procV);
             
             time+=0.1;
-            if(time >= 10) System.exit(0);
+            if(procV >= 0.9 && procV <= 1.1 && time >= 10) System.exit(0);
         }
     }
 }
